c97948d51c1c313dcf9bf738829b0c24d5da36dc
IwamotoW <32658908+IwamotoW@users.noreply.github.com>
2017-10-25 16:22:41 +0900

    feat(Glupfile.js)  Glupfileの作成#15

---
+++ Gulpfile.js
@@ -0,0 +1,67 @@
+'use strict';
+
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let imagemin = require('gulp-imagemin');
+let browserSync = require('browser-sync').create();
+let pngquant = require('imagemin-pngquant');
+let mozjpeg = require('imagemin-mozjpeg');
+let watch = require('gulp-watch');
+let plumber = require('gulp-plumber');
+
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+    },
+  });
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('imagemin', function() {
+  return gulp.src('./src/images/**/*')
+    .pipe(imagemin([
+      pngquant({
+        quality: '65-80',
+        speed: 1,
+      }),
+      mozjpeg({
+        quality: 80,
+      }),
+      imagemin.svgo(),
+      imagemin.gifsicle(),
+    ]))
+    .pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('watch', ['js','sass','imagemin'],  function() {
+  
+  watch(['./src/sass/**/*.scss'], function() {
+    gulp.start('sass');
+  });
+  watch(['./src/images/**/*'], function() {
+    gulp.start('imagemin');
+  });
+  watch(['./src/scripts/**/*.js'], function() {
+    gulp.start('js');
+  });
+  watch(['./dist/**/*'], function() {
+    browserSync.reload();
+  });
+});
+
+gulp.task('default', ['watch'], function() {
+  gulp.start('serve');
+});
