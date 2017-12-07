65f19d25f3a90f1dc8b5f0879571b5b895e6c726
yuFujii <32005523+yuFujii@users.noreply.github.com>
2017-12-08 06:32:40 +0900

    feat(carousel):カルーセル実装 #92

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,16 +1,22 @@
 $(function() {
+  if (window.innerWidth < 600) {
+    for (let index = 0; index < 3; index++) {
+      $('.js-carousel-' + index).wrapAll('<div class=""></div>');
+    }
+
     var slicks = $('.carousel').slick({
     infinite: true, // スライドをループさせるか
-    autoplay: true,
-    autoplaySpeed: 3000,
+    autoplay: false,
     dots: true,
     slidesToShow: 1,
     appendArrows: $('#arrows')
     });
+    //スライドボタン
     $('.slick-next').on('click', function () {
       slicks.slick('slickNext');
     });
     $('.slick-prev').on('click', function () {
       slicks.slick('slickPrev');
     });
+  }
 });
