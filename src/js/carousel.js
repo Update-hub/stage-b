4f92876a5182e70b9a533019652943d31f8a32db
yuFujii <32005523+yuFujii@users.noreply.github.com>
2017-12-08 08:10:20 +0900

    refactor(carousel):クラス名の変更、不要になったスクリプトの削除　#92

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -3,20 +3,12 @@ $(function() {
     for (let index = 0; index < 3; index++) {
       $('.js-carousel-' + index).wrapAll('<div class=""></div>');
     }
-
-    var slicks = $('.carousel').slick({
+    let slicks = $('.js-carousel').slick({
     infinite: true, // スライドをループさせるか
     autoplay: false,
     dots: true,
     slidesToShow: 1,
     appendArrows: $('#arrows')
-    });
-    //スライドボタン
-    $('.slick-next').on('click', function () {
-      slicks.slick('slickNext');
-    });
-    $('.slick-prev').on('click', function () {
-      slicks.slick('slickPrev');
     });
   }
 });
